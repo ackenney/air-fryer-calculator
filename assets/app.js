f172c489aec09db0ab4a3c74fc7b930b50383c01
ackenney <ackenney@gmail.com>
2024-03-12 12:30:36 -0400

    Added input error handling

--- assets/app.js
+++ assets/app.js
@@ -19,6 +19,20 @@ document.getElementById("calculateButton").addEventListener("click", function() 
     var airTemp = newTemp.toFixed(0);
     var airTime = newTime.toFixed(0);
 
+
+    if (ovenTemp > 475 || ovenTemp < 120) {
+        errorType = "Keep temperature in the range of 120°f - 475°f";
+        newTemp = 0;
+        newTime = 0;
+    } 
+    else if (ovenTime >= 120 ) {
+        errorType = "Cook times should not exceed 2 hours";
+        newTemp = 0;
+        newTime = 0;
+    }else {
+        errorType = "";
+    }
+
     document.getElementById("airTemp").textContent = "Air Fry Temp: " + airTemp + " °f";
     document.getElementById("airTime").textContent = "Air Fry Time: " + airTime + " minutes";
     document.getElementById("errorType").textContent = errorType;
